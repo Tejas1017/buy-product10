4cada69870000cf0bfd9b27dfd18d35f01162a01
Tejas1017 <tvyas7239@gmail.com>
2022-10-13 15:04:58 +0530

    master

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { __values } from 'tslib';
 import { DesignutilityService } from '../appServices/designutility.service';
 import { Auth } from '../auth.service';
 
@@ -11,44 +12,45 @@ import { Auth } from '../auth.service';
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private _cart:DesignutilityService,
-    private router:Router,
-    private route:ActivatedRoute, private authService: Auth) {
+  constructor(private _cart: DesignutilityService,
+    private router: Router,
+    private route: ActivatedRoute, private authService: Auth) {
     this.abc = this._cart.cart;
-   }
-  
-   abc : any = [];
+  }
+
+  abc: any = [];
 
   ngOnInit(): void {
   }
-  productSelected: boolean= false
+  productSelected: boolean = false
+
   selectedProduct = ''
- 
+
   select = ''
-  name:boolean = false
-  addedProduct:any
-  
-  productedOncart(value:any){
-    
-    this.productSelected =true
-    this.selectedProduct = value
-  
-    this.abc.push(this.selectedProduct)
-    
+  name: boolean = false
+  addedProduct: any
 
-    console.log(this.abc)
-    
-
-  }
-  buttonName(){
+  buttonName() {
     this.name = true
   }
-  
-  checkout(){
 
-   this.router.navigate(['cart'])
-   this.authService.login()
-
+  checkout() {
+    this.router.navigate(['cart'])
+    this.authService.login()
   }
- 
+
+  inputnumber = 0;
+
+  plus(value: any) {
+    this.productSelected = true
+    this.inputnumber = this.inputnumber + 1;
+    this.selectedProduct = value
+    this.abc.push(this.selectedProduct)
+    console.log(this.abc)
+  }
+  minus() {
+    if (this.inputnumber != 0) {
+      this.inputnumber = this.inputnumber - 1;
+    }
+  }
 }
